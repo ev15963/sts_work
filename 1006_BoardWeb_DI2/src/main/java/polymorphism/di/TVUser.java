bc6a81c1df8f2c37e5022a548c1a17d75eeb7344
ev15963 <ev15963@hs.ac.kr>
2020-10-06 20:22:58 +0900

    singleton 까지 20:22

--- 1006_BoardWeb_DI2/src/main/java/polymorphism/di/TVUser.java
+++ 1006_BoardWeb_DI2/src/main/java/polymorphism/di/TVUser.java
@@ -1,16 +1,38 @@
 package polymorphism.di;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TVUser {
 	public static void main(String[] args) {
-		BeanFactory factory = new BeanFactory(); //BeanFactory : samsung or TV 선택하게
-
-		TV tv = (TV)factory.getBean(args[0]);
 		
-//		TV tv = new SamsungTV();
-		tv.powerOn();
-		tv.volumeUp();
-		tv.volumeDown();
-		tv.powerOff();
+		//1.  Spring 컨테이너를 구동
+		AbstractApplicationContext factory = 
+				new GenericXmlApplicationContext("applicationContext.xml");
+		
+		//2. Spring 컨테이너로부터 필요한 객체 요청(Lookup)
+//		TV tv=(TV)factory.getBean("tv");
+//		tv.powerOn();
+//		tv.volumeUp();
+//		tv.volumeDown();
+//		tv.powerOff();
+
+		TV tv1 = (TV)factory.getBean("tv");
+		TV tv2 = (TV)factory.getBean("tv");
+		TV tv3 = (TV)factory.getBean("tv");
+		
+		//3. Spring 컨테이너 종료
+		factory.close();
+		
+//		BeanFactory factory = new BeanFactory(); //BeanFactory : samsung or TV 선택하게
+//
+//		TV tv = (TV)factory.getBean(args[0]);
+//		
+////		TV tv = new SamsungTV();
+//		tv.powerOn();
+//		tv.volumeUp();
+//		tv.volumeDown();
+//		tv.powerOff();
 		
 		//그냥 실행시 
 //		Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 0
