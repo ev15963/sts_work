7d93da1ceb765975e820439a9417e7bfda7d6e2f
ev15963 <ev15963@hs.ac.kr>
2020-10-13 09:26:32 +0900

    AOP 설정 09:26

---
+++ 1013_BoardWeb_DI_AOP/src/main/java/com/lsw/biz/board/common/LogAdvice.java
@@ -0,0 +1,19 @@
+package com.lsw.biz.board.common;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+public class LogAdvice {
+
+	public LogAdvice() {
+	}
+
+	@Pointcut("execution(* com.lsw.biz..*Impl.*(..))")
+	public void allPointcut(){} //객체가 만들어지고 엮음
+	
+	@Before("allPointcut()")
+	public void printLog(JoinPoint jp) {
+		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
+	}
+}
