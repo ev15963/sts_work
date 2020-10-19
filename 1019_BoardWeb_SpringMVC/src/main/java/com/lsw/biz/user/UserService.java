6d2a2c70c4f768685370de840393721a87296c62
ev15963 <ev15963@hs.ac.kr>
2020-10-19 09:13:07 +0900

    SpringMVC 출력 09:12

---
+++ 1019_BoardWeb_SpringMVC/src/main/java/com/lsw/biz/user/UserService.java
@@ -0,0 +1,6 @@
+package com.lsw.biz.user;
+
+public interface UserService {
+	//crud 기능 메소드 구현
+	public UserVO getUser(UserVO vo);
+}
---
+++ 1019_BoardWeb_SpringMVC/src/main/java/com/lsw/view/user/LogoutController.java
@@ -0,0 +1,27 @@
+package com.lsw.view.user;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.lsw.view.controller.Controller;
+
+public class LogoutController implements Controller {
+
+	@Override
+	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		// else if (path.equals("/logout.do")) {
+		System.out.println("로그아웃처리");
+
+		// logout_proc.jsp
+		// 1. 브라우져와 연결된 세션 객체를 강제 종료 한다.
+		HttpSession session = request.getSession();
+		session.invalidate();
+
+		// 2. 세션 종료후, 메인 화면으로 이동한다.
+		// response.sendRedirect("login.jsp");
+
+		return "login";
+	}
+
+}
