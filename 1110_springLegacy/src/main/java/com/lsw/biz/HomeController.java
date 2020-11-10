e9253a932936158ad7c4c46f28603f0c7d2062cf
ev15963 <ev15963@hs.ac.kr>
2020-11-10 15:59:26 +0900

    homecontroller,jsp 관련 설몇 15:59

--- 1110_springLegacy/src/main/java/com/lsw/biz/HomeController.java
+++ 1110_springLegacy/src/main/java/com/lsw/biz/HomeController.java
@@ -33,6 +33,20 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
+		
+		@RequestMapping("/")
+		public String home(Model model) {
+			logger.info("Welcome home! The client locale is {}.", locale);
+			
+			Date date = new Date();
+			DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+			
+			String formattedDate = dateFormat.format(date);
+			
+			model.addAttribute("serverTime", formattedDate );
+			
+		
+		
 		return "home";
 	}
 	
