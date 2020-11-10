fdc5f7f4d5283c13eb3581c9905d353f1f6a85c1
ev15963 <ev15963@hs.ac.kr>
2020-11-10 09:47:44 +0900

    mybatis 연동중 pom.xml sql-map-config.xml applicationContext.xml 09:47

---
+++ 1110_BoardWeb_SpringMVC_integration2/src/main/java/com/lsw/biz/board/common/BeforeAdvice.java
@@ -0,0 +1,26 @@
+package com.lsw.biz.board.common;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Service;
+
+@Service
+@Aspect
+public class BeforeAdvice {
+
+	public BeforeAdvice() {
+	}
+
+//	@Pointcut("execution(* com.lsw.biz..*Impl.*(..))")
+//	public void allPointCut() {
+//	}
+
+	@Before("PointcutCommon.allPointCut()")
+	public void beforeLog(JoinPoint jp) {
+		String method = jp.getSignature().getName();
+		Object[] args = jp.getArgs();
+
+		System.out.println("[사전처리] 11111" + method + "() 메소드 ARGS 정보 : " + args[0].toString());
+	}
+}
