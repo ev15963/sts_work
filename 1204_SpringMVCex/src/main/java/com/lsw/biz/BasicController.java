4218efb3dcfb33d62ae845982f30c4ecfe2d7a76
ev15963 <ev15963@hs.ac.kr>
2020-12-04 19:16:44 +0900

    1204_Spring MVC ex home 출력 16

---
+++ 1204_SpringMVCex/src/main/java/com/lsw/biz/BasicController.java
@@ -0,0 +1,15 @@
+package com.lsw.biz;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+	
+	@RequestMapping(value="/welcome")
+	@ResponseBody
+	public String welcome() {
+		return "Welcome to Spring MVC";
+	}
+}
